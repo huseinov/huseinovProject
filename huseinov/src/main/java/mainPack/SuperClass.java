22c5adec07443e64a0a4a2941ad26a2f06677ae8
huseinov <pashayev77@gmail.com>
2016-11-08 19:50:27 +0200

    Update SuperClass.java

--- huseinov/src/main/java/mainPack/SuperClass.java
+++ huseinov/src/main/java/mainPack/SuperClass.java
@@ -4,6 +4,7 @@ package mainPack;
 public class SuperClass {
 
     public static void main(String args[]) {
-        System.out.println("Let's start");
+        int age = 30;
+        System.out.println("I'm "+ age +" years old.");
     }
 }
